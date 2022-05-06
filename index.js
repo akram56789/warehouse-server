1b09f69a3e83f41859b3dab661ed0b7902f65e65
akram56789 <akramhossain56789@gmail>
2022-05-06 21:28:08 +0600

    product add/post created

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const res = require('express/lib/response');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -22,6 +22,19 @@ async function run() {
         const cursor = productCollection.find(query);
         const products = await cursor.toArray();
         res.send(products)
+       });
+
+       app.get('/product/:id', async(req, res) =>{
+           const id = req.params.id;
+           const query = {_id: ObjectId(id)};  
+           const product = await productCollection.findOne(query)
+           res.send(product)
+       });
+
+       app.post('/product', async(req, res)=>{
+           const newProduct = req.body;
+           const result = await productCollection.insertOne(newProduct);
+           res.send(result);
        })
        
 
